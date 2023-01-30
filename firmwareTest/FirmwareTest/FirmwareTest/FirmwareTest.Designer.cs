61f77630351f11115d13dab2cfb9346594308657
Koen Bijttebier <Koen.bijttebier@telenet.be>
2023-01-30 23:06:02 +0100

    - Firmware test tool

---
+++ firmwareTest/FirmwareTest/FirmwareTest/FirmwareTest.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace FirmwareTest
+{
+    partial class FirmwareTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edtIp = new System.Windows.Forms.TextBox();
+            this.edtLog = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // edtIp
+            // 
+            this.edtIp.Location = new System.Drawing.Point(27, 26);
+            this.edtIp.Name = "edtIp";
+            this.edtIp.Size = new System.Drawing.Size(228, 22);
+            this.edtIp.TabIndex = 0;
+            this.edtIp.Text = "192.168.10.125";
+            // 
+            // edtLog
+            // 
+            this.edtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edtLog.Location = new System.Drawing.Point(27, 75);
+            this.edtLog.Multiline = true;
+            this.edtLog.Name = "edtLog";
+            this.edtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.edtLog.Size = new System.Drawing.Size(761, 363);
+            this.edtLog.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(262, 26);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(107, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Start Test";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // FirmwareTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.edtLog);
+            this.Controls.Add(this.edtIp);
+            this.Name = "FirmwareTest";
+            this.Text = "Firmware TCP Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edtIp;
+        private System.Windows.Forms.TextBox edtLog;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ firmwareTest/FirmwareTest/FirmwareTest/FirmwareTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirmwareTest
+{
+    public partial class FirmwareTest : Form
+    {
+        private List<Thread> mWorkerThreads = null;
+        public FirmwareTest()
+        {
+            InitializeComponent();
+        }
+        public void AddLog(int threadId, string log)
+        {
+            this.Invoke(new MethodInvoker(() => { edtLog.AppendText($"[{DateTime.Now}][{threadId}]: {log}\r\n"); }));
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            const int THREAD_COUNT = 3;
+            if (mWorkerThreads == null)
+            {
+                mWorkerThreads = new List<Thread>();
+                for (int i = 0; i < THREAD_COUNT; i++)
+                {
+                    var worker = new Thread(workerThread);
+                    worker.Start(i);
+                    mWorkerThreads.Add(worker);
+                }
+            }
+        }
+        private void workerThread(object parameter)
+        {
+            int threadId = (int)parameter;
+
+            try
+            {
+
+                while (true)
+                {
+                    AddLog(threadId, "Starting Connection");
+                    int sendCount = 10;
+                    TcpClient client = new TcpClient();
+                    client.Connect(edtIp.Text, 1234);
+                    NetworkStream networkStream = client.GetStream();
+                    StreamReader reader = new StreamReader(networkStream);
+                    StreamWriter writer = new StreamWriter(networkStream);
+                    while (sendCount > 0)
+                    {
+                        sendCount--;
+                        if (client.Connected)
+                        {
+                            writer.Write("GET#");
+                            writer.Flush();
+
+                            Thread.Sleep(100);
+                            var buffer = new char[512];
+                            int length = reader.Read(buffer, 0, buffer.Length);
+                            string received = "";
+                            for (int i = 0; i < length; i++)
+                            {
+                                received += buffer[i];
+                            }
+
+                            AddLog(threadId, received + "\n");
+
+                            Thread.Sleep(100);
+                        }
+                    }
+                    client.Close();
+                    AddLog(threadId, "Done");
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog(threadId, ex.Message);
+            }
+
+        }
+    }
+}
